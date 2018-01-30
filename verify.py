a1057d62b3aa9a78659a0cd4f391fde45f90c97f
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2018-01-30 10:08:03 -0800

    Verify script working

--- verify.py
+++ verify.py
@@ -10,9 +10,39 @@ from matching_algo import *
 
 # start here
 if __name__ == "__main__":
-	preferences = pandas.read_csv(CSV_NAME, parse_dates=["Timestamp"])
-	preferences = preferences.groupby(SUID).last().reset_index()
-
+	preferences = data
 	assignments = pandas.read_excel(OUTPUT_FILENAME2)
 
+	STUDENTS_WHO_GOT_THEIR_TOP5CHOICES = 0
+	STUDENTS_WHO_GOT_RANDOM_GRADERS = 0
+	STUDENTS_WHO_GOT_BAD_GRADERS = 0
+	STUDENTS_COUNT = 0
 
+	for index, student in assignments.iterrows():
+		STUDENTS_COUNT += 1
+
+		assigned_grader = student["Assigned grader"]
+		student_email = student["Email"].split("@stanford.edu")[0]
+		student_choice_row = preferences.loc[preferences["What is your SUNetID?"] == student_email]
+
+		if len(student_choice_row) == 0:
+			# this student didn't fill the preference form
+			continue
+
+		grader_top5 = [student_choice_row[g].tolist()[0] for g in GRADER_CHOICE_LIST]
+		excluded_graders = [student_choice_row[g].tolist()[0] for g in EXCLUDED_GRADERS_LIST]
+
+		if (assigned_grader in grader_top5):
+			STUDENTS_WHO_GOT_THEIR_TOP5CHOICES += 1
+		else:
+			STUDENTS_WHO_GOT_RANDOM_GRADERS += 1
+
+		if (assigned_grader in excluded_graders):
+			STUDENTS_WHO_GOT_BAD_GRADERS += 1
+			print "ERROR:", student["Student Name"], "got a grader who they didn't want!!!"
+
+	# All done. Print stats
+	print STUDENTS_COUNT, "students in total"
+	print STUDENTS_WHO_GOT_THEIR_TOP5CHOICES, "students got one of their top 5 graders"
+	print STUDENTS_WHO_GOT_RANDOM_GRADERS, "students got a random grader"
+	print STUDENTS_WHO_GOT_BAD_GRADERS, "got a grader they didn't want"
