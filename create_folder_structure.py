a3d5c0a351da6a0028acb7b312d4a98c1ee74ad1
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2018-02-13 08:45:29 -0800

    Create folder structure for googledocs

--- create_folder_structure.py
+++ create_folder_structure.py
@@ -2,8 +2,13 @@
 # for STLP graders to correct
 # This will be uploaded to google drive
 import pandas, os
+from shutil import copyfile
 
 BASE_FOLDER = './STLPs'
+GRADING_TEMPLATE_FILE = 'GEMWIN18.STLP_gradesheet.xls'
+
+student_names = {}
+total_stlps = 0
 
 # read the document that has grader --> student name
 GRADER_ASSIGNMENTS_FILE = "GEMWIN18_grader_assignments.xlsx"
@@ -12,10 +17,17 @@ xl = pandas.ExcelFile(GRADER_ASSIGNMENTS_FILE)
 
 if __name__ == "__main__":
 	for grader in xl.sheet_names:
-		print "creating folder", grader
-		os.makedirs(os.path.join(BASE_FOLDER, grader))
+		grader_folder_name = grader + " STLPs for GEMWIN18"
+		os.makedirs(os.path.join(BASE_FOLDER, grader_folder_name))
+		copyfile(GRADING_TEMPLATE_FILE, os.path.join(BASE_FOLDER, grader_folder_name, GRADING_TEMPLATE_FILE))
 		
 		students = pandas.read_excel(xl, grader, header=1)["STUDENT NAME"].tolist()
+		print grader, ":", len(students), "students"
 		for student in students:
-			os.makedirs(os.path.join(BASE_FOLDER, grader, student))
+			# create subdirectory with student name
+			os.makedirs(os.path.join(BASE_FOLDER, grader_folder_name, student))
+			student_names[student] = 1
+			total_stlps += 1
 
+	print "total students =", len(student_names.keys())
+	print "total STLPs to be graded =", total_stlps
